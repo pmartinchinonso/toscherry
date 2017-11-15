c5a6b816029b3d7c67085be2abcf06cbcd099bc6
pmartinchinonso <promisemary10@gmail.com>
2017-11-15 13:54:17 +0100

    adding to_home link to the logo image and text

--- toscherry_header.php
+++ toscherry_header.php
@@ -12,21 +12,21 @@
         <div class="container">
         
           <div id="logo" class="pull-left">
-            <a href="#hero"><img src="img/toscherry_logo.png" alt="Toscherry" title="toscherry" /></img></a>
-            <h1 class="font-effect-3d"><a href="#hero">Toscherry</a></h1>
+            <a href="index.php"><img src="img/toscherry_logo.png" alt="Toscherry" title="toscherry" /></img></a>
+            <h1 class="font-effect-3d"><a href="index.php">Toscherry</a></h1>
           </div>
             
           <div id="navi-menu-container">
             <ul class="navi-menu">
               <li class="<?php echo $home_active;?>"><a href="index.php">Home</a></li>
-              <li class="<?php echo $about_active;?>"><a href="#about">About Us</a></li>
+              <li class="<?php echo $about_active;?>"><a href="about.php">About Us</a></li>
               <li class="<?php echo $team_active;?>"><a href="index.php#subscribe">Subscribe</a></li>
               <li class="menu-has-children <?php echo $products_active;?>"><a href="">Products &#x25BE;</a>
                 <ul>
                   <li><a href="computers.php">Computers</a></li>
                   <li><a href="printers.php">Printers</a></li>
                   <li><a href="plastics.php">Exotic Plastics</a></li>
-                  <li><a href="gadgets.php">Security Gadgets</a></li>
+                  <li><a href="bathroom.php">Bathroom Solutions</a></li>
                 </ul>
               </li>
               <li class="<?php echo $contact_active;?>"><a href="contact.php">Contact Us</a></li>
