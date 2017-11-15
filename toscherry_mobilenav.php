1b46e0be7667ea08a9e72502814b9a7dd5de798b
pmartinchinonso <promisemary10@gmail.com>
2017-11-15 13:54:45 +0100

    linking the about us page in mobile_nav

--- toscherry_mobilenav.php
+++ toscherry_mobilenav.php
@@ -2,7 +2,7 @@
 	<div class="side_nav">
         <button class="close_button" onClick="side_close()">&times;</button>
         <a href="index.php" class="<?php echo $home_active;?>">Home</a>
-        <a href="#" class="<?php echo $about_active;?>">About Us</a>
+        <a href="about.php" class="<?php echo $about_active;?>">About Us</a>
         <a href="index.php#subscribe" class="<?php echo $team_active;?>">Subscribe</a>
         <div class="side_menu <?php echo $products_active;?>">
             <div id="side_item" class="<?php echo $products_active;?>">Products &#x25BE;</div>
@@ -10,7 +10,7 @@
                 <a href="computers.php">Computers</a>
                 <a href="printers.php">Printers</a>
                 <a href="plastics.php">Exotic Plastics</a>
-                <a href="gadgets.php">Security Gadgets</a>
+                <a href="bathroom.php">Bathroom Solutions</a>
             </div>
         </div>
         <a href="contact.php" class="<?php echo $contact_active;?>">Contact Us</a>
